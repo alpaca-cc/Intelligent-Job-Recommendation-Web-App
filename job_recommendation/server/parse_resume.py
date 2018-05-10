91d720d1a669d44b26265e5f07f3d462a907be98
alpaca-cc <chchen146@gmail.com>
2018-05-10 01:13:46 -0500

    resume parsing, words processing etc.

--- job_recommendation/server/parse_resume.py
+++ job_recommendation/server/parse_resume.py
@@ -1,8 +1,46 @@
 
 import textract
-# text = textract.process('files/resume.pdf')
-# print (text)
+import metapy
+
+
+def process_pdf(path):
+    text = textract.process(path)
+    # print(text)
+    lines = text.split('\n')
+    # print(lines)
+    # for line in text:
+    #     print (line)
+    return text
+
+
+def preprocess_resume_text(text):
+    content_list = text.split('\n')
+
+
+def metapy_process(text, n):
+    doc = metapy.index.Document()
+    doc.content(text)
+    tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
+    tok = metapy.analyzers.ListFilter(tok, "supporting_files/stopwords.txt", metapy.analyzers.ListFilter.Type.Reject)
+    tok = metapy.analyzers.LengthFilter(tok, min=1, max=30)
+    tok = metapy.analyzers.LowercaseFilter(tok)
+    tok = metapy.analyzers.Porter2Filter(tok)
+    ana = metapy.analyzers.NGramWordAnalyzer(n, tok)
+    tok.set_content(doc.content())
+    ngram = ana.analyze(doc)
+
+    tokens, counts = [], []
+    for token, count in ngram.items():
+        counts.append(count)
+        tokens.append(token)
+    return tokens
 
 if __name__ == "__main__":
-    text = textract.process('files/resume.pdf')
-    print (text)
+    # text = process_pdf('files/resume.pdf')
+    text = process_pdf('files/resume3.docx')
+    text = unicode(text, 'utf-8')
+    # type(text)
+    tokens = metapy_process(text, 3)
+    print(tokens)
+
+    # print (text)
